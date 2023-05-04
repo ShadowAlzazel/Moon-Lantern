96fdbc2bcc24715334dd023508802127dd57c250
ShadowAlzazel <pastenalfredojr@gmail.com>
2023-05-03 17:06:37 -0700

    player sprite

--- src/player.py
+++ src/player.py
@@ -1,17 +1,40 @@
 import pygame
 from settings import *
+from support import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
+        # Assets
+        self.import_assets()
+        # Sprite
+        self.status = 'down_idle'
+        self.frame_index = 0
+
         # Image in W x H
-        self.image = pygame.Surface((32, 64))
-        self.image.fill("Green")
+        print(self.animations)
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center=pos)
         # Movement
         self.direction = pygame.math.Vector2(0, 0)
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 160
+
+    def import_assets(self):
+        # TODO: Use Maps and Asset Postproccessor!
+
+        # These are the character sub folders
+        self.animations = {
+            'up': [], 'down': [], 'left': [], 'right': [],
+            'up_idle': [], 'down_idle': [], 'left_idle': [], 'right_idle': []
+        }
+
+        for animation in self.animations.keys():
+            # Note in moon_lantern dir use below
+            full_path = f'assets/character/{animation}'
+            # if in src directory use below
+            #full_path = f'../assets/character/{animation}'
+            self.animations[animation] = import_folder(full_path)
 
     def input(self):
         keys_pressed = pygame.key.get_pressed()
